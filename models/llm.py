8ad8d6124902c92e3c9ce55661523c33e2e2f5af
Venkata Suneel G <suneelvenkat112@gmail.com>
2026-04-11 13:22:46 +0530

    Update llm.py

--- models/llm.py
+++ models/llm.py
@@ -1,6 +1,9 @@
 import google.generativeai as genai
 
-def ask_gemini(prompt: str, model: str = "models/gemini-1.5-flash-latest") -> str:
-    llm = genai.GenerativeModel(model)
-    response = llm.generate_content(prompt)
-    return response.text
+def ask_gemini(prompt: str, model: str = "gemini-1.0-pro") -> str:
+    try:
+        llm = genai.GenerativeModel(model)
+        response = llm.generate_content(prompt)
+        return response.text
+    except Exception as e:
+        return f"ERROR: {str(e)}"
